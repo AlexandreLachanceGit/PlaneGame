92d5c77e68b3db7f5dba3d3b4226ece145b21a5b
Alex <alexandrelachance@me.com>
2021-11-22 21:52:23 -0500

    Added regenerate button.

--- src/world_gen/gen.rs
+++ src/world_gen/gen.rs
@@ -8,15 +8,14 @@ impl Plugin for WorldGen {
         let now = std::time::Instant::now();
 
         app.add_startup_system(setup.system())
-            .add_plugin(world_gen::ui_controls::UIControls)
-            .add_system(world_gen_data_changed.system());
+            .add_plugin(world_gen::ui_controls::UIControls);
 
         let elapsed_time = now.elapsed();
         println!("World generated in {} seconds.", elapsed_time.as_secs_f32());
     }
 }
 
-struct ChunkData {
+pub struct ChunkData {
     pos_x: i32,
     pos_y: i32,
     size: i32,
@@ -63,54 +62,41 @@ fn setup(
     });
 }
 
-fn world_gen_data_changed(
+pub fn regenerate(
     mut commands: Commands,
     mut chunk_query: Query<(&mut ChunkData, &mut Transform, &Handle<Mesh>, Entity)>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    world_gen_data: Res<world_gen::WorldGenData>,
+    world_gen_data: &ResMut<world_gen::WorldGenData>,
 ) {
-    if world_gen_data.is_changed() {
-        for (_chunk_data, _transform, _mesh, entity) in chunk_query.iter_mut() { 
-            commands.entity(entity).despawn();
-        }
+    for (_chunk_data, _transform, _mesh, entity) in chunk_query.iter_mut() {
+        commands.entity(entity).despawn();
+    }
 
-        for x in 0..world_gen_data.size {
-            for y in 0..world_gen_data.size {
-                let chunk_size = world_gen_data.chunk_size;
-                commands
-                    .spawn()
-                    .insert(ChunkData {
-                        pos_x: x,
-                        pos_y: y,
-                        size: chunk_size,
-                    })
-                    .insert_bundle(PbrBundle {
-                        mesh: meshes.add(world_gen::chunk_gen::create_square_mesh(chunk_size)),
-                        material: if (y + x) % 2 == 0 {
-                            materials.add(Color::WHITE.into())
-                        } else {
-                            materials.add(Color::BLACK.into())
-                        },
-                        transform: Transform::from_xyz(
-                            (x * chunk_size) as f32,
-                            0.0,
-                            (y * chunk_size) as f32,
-                        ),
-                        ..Default::default()
-                    });
-            }
+    for x in 0..world_gen_data.size {
+        for y in 0..world_gen_data.size {
+            let chunk_size = world_gen_data.chunk_size;
+            commands
+                .spawn()
+                .insert(ChunkData {
+                    pos_x: x,
+                    pos_y: y,
+                    size: chunk_size,
+                })
+                .insert_bundle(PbrBundle {
+                    mesh: meshes.add(world_gen::chunk_gen::create_square_mesh(chunk_size)),
+                    material: if (y + x) % 2 == 0 {
+                        materials.add(Color::WHITE.into())
+                    } else {
+                        materials.add(Color::BLACK.into())
+                    },
+                    transform: Transform::from_xyz(
+                        (x * chunk_size) as f32,
+                        0.0,
+                        (y * chunk_size) as f32,
+                    ),
+                    ..Default::default()
+                });
         }
-        /*for (mut chunk_data, mut transform, mesh) in chunk_query.iter_mut() {
-            meshes.set_untracked(
-                mesh,
-                world_gen::chunk_gen::create_square_mesh(world_gen_data.chunk_size),
-            );
-            transform.translation = Vec3::new(
-                (chunk_data.pos_x * world_gen_data.chunk_size) as f32,
-                0.0,
-                (chunk_data.pos_y * world_gen_data.chunk_size) as f32,
-            );
-        }*/
     }
 }
--- src/world_gen/ui_controls.rs
+++ src/world_gen/ui_controls.rs
@@ -10,18 +10,32 @@ impl Plugin for UIControls {
     }
 }
 
-fn world_gen_ui(egui_context: ResMut<EguiContext>, mut world_gen_data: ResMut<world_gen::WorldGenData>) {
+fn world_gen_ui(
+    commands: Commands,
+    chunk_query: Query<(
+        &mut world_gen::gen::ChunkData,
+        &mut Transform,
+        &Handle<Mesh>,
+        Entity,
+    )>,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+    egui_context: ResMut<EguiContext>,
+    mut world_gen_data: ResMut<world_gen::WorldGenData>,
+) {
     egui::Window::new("World gen params").show(egui_context.ctx(), |ui| {
         let mut size = world_gen_data.size;
         let mut chunk_size = world_gen_data.chunk_size;
 
-        ui.add(egui::Slider::new(&mut size, 1..=20).text("Size"));
-        ui.add(egui::Slider::new(&mut chunk_size, 1..=20).text("Chunk size"));
+        ui.add(egui::Slider::new(&mut size, 1..=200).text("Size"));
+        ui.add(egui::Slider::new(&mut chunk_size, 1..=200).text("Chunk size"));
 
-        if world_gen_data.chunk_size != chunk_size {
-            world_gen_data.chunk_size = chunk_size;
+        if ui.button("Regenerate").clicked() {
+            world_gen::gen::regenerate(commands, chunk_query, meshes, materials, &world_gen_data);
         }
-        if world_gen_data.size != size {
+
+        if world_gen_data.chunk_size != chunk_size || world_gen_data.size != size {
+            world_gen_data.chunk_size = chunk_size;
             world_gen_data.size = size;
         }
     });
